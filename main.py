f59f22f8e32b759a0d70a63f29a97a5efa6454df
MassiTheFallen <massimoquadri@gmail.com>
2025-07-03 00:42:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 import os
+
 TOKEN = "7173358240:AAGwChnArLqKi71yzQRhWOXlBbPVA-ScvfE"
 
 logging.basicConfig(
